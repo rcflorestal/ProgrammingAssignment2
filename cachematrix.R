eeb0b77877e7f76d6b8864e19c9e70d39f444860
Robson Cruz <rcflorestal@yahoo.com.br>
2020-11-18 17:11:54 -0300

    cacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##----------------------Programming Assignment 2------------------------------##
+## creates two functions, one stores the elements of a cached array and the   ##
+## other returns the inverse of the array.                                    ## 
+##----------------------Programming Assignment 2------------------------------##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()){
+                i <- NULL
+                set <- function(y){
+                        x <<- y
+                        i <<- NULL
+                }
+                get <- function() x
+                set_i <- function(solve) i <<- solve
+                get_i <- function() i
+                list(get = get , set_i= set_i, get_i = get_i)
 }
 
+## Call the makeCacheMatrix() function 
+makeCacheMatrix(matrix(c(1, 0, 0, 2, 1, 0, 3, 4, 1), nrow = 3, ncol = 3))
 
-## Write a short comment describing this function
+# This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated 
+# (and the matrix has not changed), then the cachesolve should retrieve the 
+# inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+        i <- x$get_i()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }else{
+                data <- x$get()
+                i <- solve(data, ...)
+                x$set_i(i)
+                return(i)
+        }
 }
+
+## Cal the cacheSolve() fucntion
+cacheSolve(cacheMatrix)
